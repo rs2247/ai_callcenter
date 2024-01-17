c5a877a0536df00932a514d72c4152d8dc81c539
Renato Sanabria <sanabria@MacBook-Air-de-Renato.local>
2024-01-17 02:22:14 -0300

    modified the demo to not use SpeechRecognition. Also included code to detect when user started to speak and cancel ambient noise

--- playground/sana/demo.py
+++ playground/sana/demo.py
@@ -1,70 +1,111 @@
 import os
-from elevenlabs import Voice, VoiceSettings, generate,play, stream
+from elevenlabs import Voice, VoiceSettings, generate,play
+import elevenlabs
 from elevenlabs import set_api_key
 from openai import OpenAI
 import speech_recognition as sr
+import time
+import numpy as np
+import faster_whisper
+import torch.cuda
+import pyaudio, wave, collections, torch.cuda, numpy
 
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
-
-
 set_api_key("aece25f51c8252ea4541f2b1604cac3d")
-# openai.api_key = "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" ## OpenAI Key
 
 
-# audio = generate(
-#     text="Oi eu gostaria de comprar uma pizza de calabresa com tomate",
-#     voice=Voice(
-#         voice_id='EXAVITQu4vr4xnSDxMaL',
-#         settings=VoiceSettings(stability=0.4, similarity_boost=0.5, style=0.0, use_speaker_boost=True)
-#     ),
-#     model="eleven_multilingual_v1"
-# )
-
-# play(audio)
-
-################
-
-# from elevenlabs import voices, generate
-
-# voices = voices()
-# audio = generate(text="Hello there!", voice=voices[0])
-
-# for voice in voices:
-#     print(voice)
-#     print('\n')
+#Getting waiter input
+times_mic = []
+times_whisper = []
+times_openai = []
 
 
-################
+def get_levels(data, long_term_noise_level, current_noise_level):
+    pegel = numpy.abs(numpy.frombuffer(data, dtype=numpy.int16)).mean()
+    long_term_noise_level = long_term_noise_level * 0.995 + pegel * (1.0 - 0.995)
+    current_noise_level = current_noise_level * 0.920 + pegel * (1.0 - 0.920)
+    return pegel, long_term_noise_level, current_noise_level
 
-# audio_stream = generate(
-#   text="Oi eu gostaria de comprar ...... uma pizza de calabresa",
-#   stream=True,
-#   model="eleven_multilingual_v1"
-# )
-
-# stream(audio_stream)
-
-###############
 
 
 while True: 
 
+    if times_mic != []:
+        print('Average latencies:')
+        print(f'Mic input: {np.average(times_mic)}')
+        print(f'Speech to text: {np.average(times_whisper)}')
+        print(f'Open AI: {np.average(times_openai)}')
+        print("\n\n")
     
-    #Getting waiter input
-    r = sr.Recognizer()  # initialize a recognizer object
-    with sr.Microphone() as source:  # use the microphone
-        print("Fale alguma coisa :")
-        waiter_audio = r.listen(source)  # record the audio, store in audio wav file
-        # print(waiter_audio)
-        waiter_input = r.recognize_whisper(waiter_audio, language='portuguese')
+
+
+    #Old AAD AND STT
+    # r = sr.Recognizer()  # initialize a recognizer object
+    # with sr.Microphone() as source:  # use the microphone
+    #     print("Fale alguma coisa :")
+    #     tic = time.perf_counter()
+    #     waiter_audio = r.listen(source)  # record the audio, store in audio wav file
+    #     # print(waiter_audio)
+    #     toc = time.perf_counter()
+    #     times_mic.append(toc - tic)
+    #     print(f"Delta_t get input from mic {times_mic[-1]}")
+
+
+    #     tic = time.perf_counter()        
+    #     waiter_input = r.recognize_whisper(waiter_audio, language='portuguese')
+    #     toc = time.perf_counter()
+    #     times_whisper.append(toc - tic)
+    #     print(f"Delta_t whisper {times_whisper[-1]}")
+
+    # print("Atendente disse: {}".format(waiter_input))
+
+
+
+    #New AAD and STT
+    audio = pyaudio.PyAudio()
+    stream = audio.open(rate=16000, format=pyaudio.paInt16, channels=1, input=True, frames_per_buffer=512)
+    audio_buffer = collections.deque(maxlen=int((16000 // 512) * 0.5))
+    frames, long_term_noise_level, current_noise_level, voice_activity_detected = [], 0.0, 0.0, False
+
+    print("\n\nSay something. ", end="", flush=True)
+    while True:
+        data = stream.read(512)
+        pegel, long_term_noise_level, current_noise_level = get_levels(data, long_term_noise_level, current_noise_level)
+        audio_buffer.append(data)
+
+        if voice_activity_detected:
+            frames.append(data)            
+            if current_noise_level < ambient_noise_level + 100:
+                break # voice actitivy ends 
+        
+        if not voice_activity_detected and current_noise_level > long_term_noise_level + 300:
+            voice_activity_detected = True
+            print("Voice detected, starting to capture it.\n")
+            ambient_noise_level = long_term_noise_level
+            frames.extend(list(audio_buffer))
+
+    stream.stop_stream(), stream.close(), audio.terminate()        
+
+    # Transcribe recording using whisper
+    with wave.open("voice_record.wav", 'wb') as wf:
+        wf.setparams((1, audio.get_sample_size(pyaudio.paInt16), 16000, 0, 'NONE', 'NONE'))
+        wf.writeframes(b''.join(frames))
+    
+    tic = time.perf_counter()        
+    audio_file= open("voice_record.wav", "rb")    
+    waiter_input = client.audio.transcriptions.create(model="whisper-1", file=audio_file,response_format="text")
+    toc = time.perf_counter()
+    times_whisper.append(toc - tic)
+    print(f"Delta_t whisper {times_whisper[-1]}")
+
     print("Atendente disse: {}".format(waiter_input))
-    
+
 
     #Sending waiter input to GPT to get the buyer input
     # prompt = input("Usuário: ")
     # print(prompt)
+    tic = time.perf_counter()
     prompt = waiter_input
-
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -74,7 +115,12 @@ while True:
         temperature=0.7,
     )
     buyer_input = completion.choices[0].message.content
+    toc = time.perf_counter()
+    times_openai.append(toc -tic)
+    print(f"Delta_t openai {times_openai[-1]}")
     print(buyer_input)
+
+
 
     #Converting the buyer input to audio
     audio_stream = generate(
@@ -83,4 +129,4 @@ while True:
       model="eleven_multilingual_v1"
     )
 
-    stream(audio_stream)
+    elevenlabs.stream(audio_stream)
