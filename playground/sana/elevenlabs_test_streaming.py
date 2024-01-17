570843d2159a9f96ca2334b81e81a2890ce30406
Renato Sanabria <sanabria@MacBook-Air-de-Renato.local>
2024-01-17 01:37:35 -0300

    added a test of streaming the input of elevenlabs

---
+++ playground/sana/elevenlabs_test_streaming.py
@@ -0,0 +1,30 @@
+#Quando usamos a versão streamada do ElevenLabs o sotaque sai totalmente americano. Na versão sem streaming, o sotaque sai de Brasileiro
+
+from elevenlabs import generate, stream
+
+
+## VERSÃO STREAMADA ##
+def text_stream():
+    yield "O endereço é"
+    yield "Rua Mourato coelho 208"
+
+
+audio = generate(
+    text=text_stream(),
+    model='eleven_monolingual_v1',
+    stream=True, 
+    api_key = "aece25f51c8252ea4541f2b1604cac3d"
+)
+
+# Stream the audio directly 
+stream(audio)
+
+
+## VERSÃO SEM STREAMING ##
+
+audio = generate(
+  text=" O endereço é rua mourato coelho 208",
+  stream=True,
+  model="eleven_multilingual_v1"
+)
+stream(audio)
