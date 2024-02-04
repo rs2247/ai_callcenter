c612a19e333f70db91a1ef507988ec0675ab4215
Renato Sanabria <sanabria@MacBook-Air-de-Renato.local>
2024-02-04 18:47:15 -0300

    showing sales analysis with circles and quotes

--- playground/sana/sales_copilot/app/routes.py
+++ playground/sana/sales_copilot/app/routes.py
@@ -37,27 +37,44 @@ def ask_gpt(transcript):
               {transcript}
             '''
         },
+                # O vendedor conseguiu quebrar as principais objeções do aluno? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem ter 40 palavras ou mais e devem ser separados por ; 
+                # Macro        
+                #     - O vendedor conseguiu criar conexão emocional e empatia com o aluno? 
+                #     - O vendedor foi conciso e objetivo na sua fala? De uma resposta direta (Sim ou Não) e uma explicação que contenha o percentual do diálogo que foi falado pelo vendedor (entre 0 e 100%)
+
         {
             "role": "user", 
             "content": '''
+                ################################## INSTRUÇÃO #################################
                 Responda às seguintes perguntas em bullet points: 
-                Quais as motivações do aluno para procurar a DNC? Respostas devem ser separadas por ;
-                Quais as principais objeções mencionadas pelo aluno que podem impedi-lo de comprar o curso? Respostas devem ser separadas por ;
-                Qual o orçamento disponível pelo aluno para comprar o curso? 
-                Quais foram as principais dúvidas do aluno sobre o curso? Respostas devem ser separadas por ;
-                O aluno citou algum concorrente? Se sim, retorne o trecho em que o aluno fala de algum concorrente
-                O aluno decidiu comprar o curso? 
-                Quais são os próximos passos?
-                O vendedor fez perguntas para entender as necessidades do aluno? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem ter 40 palavras ou mais e devem ser separados por ; 
-                O vendedor conseguiu criar conexão emocional e empatia com o aluno? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem ter 40 palavras ou mais e devem ser separados por ; 
-                O vendedor demonstrou conhecimento sobre os cursos apresentados? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem ter 40 palavras ou mais e devem ser separados por ; 
-                O vendedor conseguiu quebrar as principais objeções do aluno? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem ter 40 palavras ou mais e devem ser separados por ; 
-                O vendedor construiu credibilidade na marca da DNC? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem ter 40 palavras ou mais e devem ser separados por ; 
-                O vendedor ofereceu um curso que coubesse no orçamento do aluno? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem ter 40 palavras ou mais e devem ser separados por ; 
-                O vendedor criou senso de urgência no aluno para que ele desse um prazo para a resposta? De uma resposta direta (Sim ou Não), uma explicação e trechos da transcrição que justifiquem a resposta. Os trechos devem 40 palavras ou mais e devem ser separados por ; 
-                O vendedor foi conciso e objetivo na sua fala? De uma resposta direta (Sim ou Não), uma explicação e diga quantos porcento do diálogo foi falado pelo vendedor
+                - Quais as motivações do aluno para procurar a DNC? Respostas devem ser separadas por ;
+                - Quais as principais objeções mencionadas pelo aluno que podem impedi-lo de comprar o curso? Respostas devem ser separadas por ;
+                - Qual o orçamento disponível pelo aluno para comprar o curso? 
+                - Quais foram as principais dúvidas do aluno sobre o curso? Respostas devem ser separadas por ;
+                - O aluno citou algum concorrente? Se sim, retorne o trecho em que o aluno fala de algum concorrente
+                - O aluno decidiu comprar o curso? 
+                - Quais são os próximos passos?
+                
+                Para as perguntas abaixo forneça uma resposta direta (Sim ou Não), uma explicação da sua resposta e trechos (um ou mais) da transcrição que justifiquem a resposta caso ela seja Sim. Os trechos devem ter 60 palavras ou mais e devem ser separados por ; 
+                Abertura
+                    - O vendedor se apresentou e cumprimentou o aluno? 
+                Discovery                    
+                    - O vendedor perguntou qual o objetivo do aluno ao comprar esse curso?
+                    - O vendedor perguntou qual curso o aluno tem interesse?
+                    - O vendedor perguntou se o aluno já fez ensino superior ou não? 
+                Pitch de vendas    
+                    - O vendedor explicou o conteúdo dos cursos e como eles atenderiam as necessidades do aluno?  
+                    - O vendedor explicou que os cursos são ministrados por professores que tem experiência de mercado e não por acadêmicos?   
+                    - O vendedor explicou que os cursos acompanham projetos práticos e não são apenas teóricos? 
+                    - O vendedor perguntou se as opções de curso apresentadas faziam sentido para o aluno?
+                Closing    
+                    - O vendedor conseguiu descobrir quanto o aluno gostaria de gastar no curso? 
+                    - O vendedor explicou o preço e as diferenças formas de pagar o curso? 
+                    - O vendedor ofereceu um curso que coubesse no orçamento do aluno? 
+                    - O vendedor tentou fechar a venda durante a ligação ou exigiu do aluno um prazo para a resposta? 
 
 
+                ################################## FORMATO DA RESPOSTA #################################
                 Responda com uma string no seguinte formato:
                 {
                     "Aluno": { 
@@ -79,62 +96,92 @@ def ask_gpt(transcript):
                         "Próximos_passos":""
                     }
                     "Vendedor":{
-                        "Perguntas":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "trechos": ""
+                        "Abertura":{
+                            "Apresentação":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            }
                         },
-                        "Conexão":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "trechos": ""
+                        "Discovery": {
+                            "Objetivo":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Curso":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Ensino_superior":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            }
                         },
-                        "Conhecimento":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "trechos": ""
+                        "Pitch de vendas":{
+                            "Conteúdo":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Professores":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Projetos":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Faz_sentido":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            }
+                        },  
+                        "Closing":{
+                            "Orçamento":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Preficicação":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Match_orçamento_curso":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            },
+                            "Fechamento":{
+                                "resposta": "Sim"/"Não",
+                                "explicação": "",
+                                "trechos": ""
+                            }
                         },
-                        "Objeções":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "trechos": ""
-                        },
-                        "Credibilidade":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "trechos": ""
-                        },
-                        "Orçamento":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "trechos": ""
-                        },
-                        "Urgência":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "trechos": ""
-                        }
-                        "Objetividade":{
-                            "resposta": "Sim"/"Não",
-                            "explicação": "",
-                            "percentual_vendedor": ""
-                        }
                     }
                }
-                '''
+            '''
         }]
 
     )
 
     print(completion.choices[0].message.content)
-    return completion.choices[0].message.content
+    return json.loads(completion.choices[0].message.content)
 
 
 
 @app.route('/')
 def index():
     transcript = load_transcript('test.txt')
-    # ask_gpt(transcript)
+    
+    # gpt_answer = ask_gpt(transcript)
+    
     gpt_answer = load_gpt_buffer('gpt_answer.txt')
     return render_template( 
         'index.html', 
